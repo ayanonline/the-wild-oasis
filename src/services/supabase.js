451cdf3c5959f544ab021971a898eb6b7e78daea
ayan <ayanghosh1389@gmail.com>
2023-08-31 01:18:00 +0530

    edit/update/copy cabin done

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rzhiudoruavjiurrcada.supabase.co";
+export const supabaseUrl = "https://rzhiudoruavjiurrcada.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6aGl1ZG9ydWF2aml1cnJjYWRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzODU0MTQsImV4cCI6MjAwODk2MTQxNH0.Tch5YgFuXUfQfiT81WSG1dO-iWmQFQBfurWYGrNUPKQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
